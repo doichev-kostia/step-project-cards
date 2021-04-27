a468fbb4162fcedce9f6108bb99cf700cb8b39c2
doichev-kostia <kostynd2004@gmail.com>
2021-04-27 16:10:51 +0300

    Changed Form and Visit sections

--- js/components/Form.js
+++ js/components/Form.js
@@ -9,7 +9,8 @@ const defaultCSSStyles = {
 }
 
 
-export default class Form{/**
+export class Form {
+    /**
      * @requires:
      * classList - string (can be array of strings),
      * attributesObj - object {attributeName: attributeValue}
@@ -27,9 +28,9 @@ export default class Form{/**
      *      placeholder - string(for placeholder attribute in input if don't need it, put empty string ""),
      *      attributesObj - object  {attributeName: attributeValue},
      *      position - object that represents the position the element needs to be inserted in {
- *          parent: DOM element,
- *          position: "afterbegin" || "beforebegin" || "afterend" || "beforeend"
- *      }
+     *          parent: DOM element,
+     *          position: "afterbegin" || "beforebegin" || "afterend" || "beforeend"
+     *      }
      *      @returns created DOM elements
      *
      * @method renderSelect():
@@ -73,51 +74,44 @@ export default class Form{/**
      *      @returns array of created DOM elements
      * */
 
-    constructor(classList = "form", attributesObj) {
+    constructor(classListObj = "form", attributesObj = {}) {
+        this.classListObj = classListObj;
         this.attributesObj = attributesObj;
-        this.classList = classList;
-        this.elements ={
-            form: new DOMElement("form", classList, "", attributesObj).render()
+        this.elements = {
+            form: new DOMElement("form", classListObj.form, "").render()
         }
     }
 
-
     renderForm(){
-        let {classList,  attributesObj, elements} = this;
+        let {classListObj,  attributesObj, elements} = this;
         return elements.form
     }
 
-    renderInput( labelText,
-                 classListObj = defaultCSSStyles,
-                 placeholder,
-                 attributesObj = {},
-                 position ={
-                    parent: this.elements.form,
-                     position: "beforeend",
-                 }
+
+    renderInput(labelText,
+                classListObj = defaultCSSStyles,
+                placeholder,
+                attributesObj = {},
     ) {
         const {elements} = this;
 
-        let { label: labelAttr , input: inputAttr} = attributesObj
+        let {label: labelAttr, input: inputAttr} = attributesObj
 
-        if(labelText){
+        if (labelText) {
             const label = new DOMElement("label", classListObj.label, labelText, labelAttr).render()
             const input = new DOMElement("input", classListObj.input, "", {
                 ...attributesObj.input,
                 placeholder,
             }).render()
 
-            position.parent.insertAdjacentElement(position.position,label);
             label.append(input)
-            return [label, input]
-        }else{
-            const input = new DOMElement("input", classListObj.input, "", {
+            return label
+        } else {
+            return new DOMElement("input", classListObj.input, "", {
                 ...inputAttr,
                 placeholder,
             }).render()
 
-            position.parent.insertAdjacentElement(position.position,input);
-            return input
         }
     }
 
@@ -126,25 +120,10 @@ export default class Form{/**
                  classListObj = defaultCSSStyles,
                  optionsTextArr,
                  attributesObj = {},
-                 position ={
-                     parent: this.elements.form,
-                     position: "beforeend",
-                 }
-    ){
+    ) {
         const {elements} = this;
         let {select: selectAttr, label: labelAttr, options: optionsAttr} = attributesObj;
-        let createdElements = []
         const select = new DOMElement("select", classListObj.select, "", selectAttr).render();
-
-        createdElements.push(select)
-
-        if (labelText){
-            const label = new DOMElement("label", classListObj.label, labelText, labelAttr).render();
-
-            label.append(select);
-            createdElements.push(label)
-            position.parent.insertAdjacentElement(position.position,label);
-        }
 
         for (let i = 0; i < optionsTextArr.length; i++) {
             const option = new DOMElement("option", classListObj.options, optionsTextArr[i], {
@@ -152,23 +131,26 @@ export default class Form{/**
                 value: optionValues[i]
             }).render();
 
-            createdElements.push(option);
             select.append(option)
         }
 
-        position.parent.insertAdjacentElement(position.position,select);
-        return createdElements
+        if (labelText) {
+            const label = new DOMElement("label", classListObj.label, labelText, labelAttr).render();
+
+            label.append(select);
+            return label
+        }
+
+
+            return select
+
     }
 
-    renderTextarea( labelText,
-                    classListObj = defaultCSSStyles,
-                    textareaDescription,
-                    attributesObj = {},
-                    position ={
-                        parent: this.elements.form,
-                        position: "beforeend",
-                    }
-    ){
+    renderTextarea(labelText,
+                   classListObj = defaultCSSStyles,
+                   textareaDescription,
+                   attributesObj = {},
+    ) {
         const {elements} = this;
 
         let {label: labelAttr, textarea: textareaAttr} = attributesObj;
@@ -178,15 +160,303 @@ export default class Form{/**
             for: textareaDescription
         }).render();
 
-        const textarea = new DOMElement("textarea", classListObj.textarea, "",{
+        const textarea = new DOMElement("textarea", classListObj.textarea, "", {
             ...textareaAttr,
             id: textareaDescription
         }).render();
 
-        position.parent.insertAdjacentElement(position.position, label);
         label.append(textarea)
 
-        return[label, textarea]
+        return label
     }
 
 }
+
+export class VisitForm extends Form {
+
+    static insertElementNextToAnotherElement(staticElement, elementToInsert) {
+        staticElement.after(elementToInsert);
+    }
+
+    renderVisitForm() {
+        const {classListObj, attributesObj, elements} = this;
+
+        elements.fullName = super.renderInput("ФИО",
+            {label: classListObj.label, input: classListObj.input},
+            "",
+            {
+                input: {
+                    required: true,
+                    name: "fullName"
+                }
+            });
+
+        elements.priority = super.renderSelect("Срочность: ",
+            ["","low", "normal", "high"],
+            {label: classListObj.label, select: classListObj.select, options: classListObj.options},
+            ["Выберите срочность: ", "Обычная", "Приоритетная", "Неотложная"],
+            {
+                select: {
+                    required: true,
+                    name: "priority"
+                }
+            });
+
+        elements.priority.children[0].children[0].disabled = true;
+
+        elements.reason = super.renderInput("Цель визита",
+            {label: classListObj.label, input: classListObj.input},
+            "",
+            {
+                input: {
+                    required: true,
+                    name: "reason"
+                }
+            });
+
+        elements.description = super.renderTextarea("Краткое описание: ",
+            {label: classListObj.label, textarea: classListObj.textarea},
+            "description",
+            {
+                textarea: {
+                    name: "description"
+                }
+            });
+
+        elements.submitButton = new DOMElement("button",
+            classListObj.button,
+            "Создать визит",
+            {type: "submit"}).render();
+
+        elements.form.append(
+            elements.fullName,
+            elements.priority,
+            elements.reason,
+            elements.description,
+            elements.submitButton)
+
+        return {
+            form: elements.form,
+            fullName: elements.fullName,
+            priority: elements.priority,
+            reason: elements.reason,
+            description: elements.description,
+            submitButton:  elements.submitButton,
+            card:{
+                fullName:{
+                    label: "ФИО: ",
+                    value: elements.fullName.children[0].value
+                },
+                priority:{
+                    label:"Срочность: ",
+                    value: elements.priority.children[0].value
+                },
+                reason: {
+                    label:"Цель визита: ",
+                    value: elements.reason.children[0].value
+                },
+                description:{
+                    label:"Описание визита: ",
+                    value: elements.description.children[0].value
+                }
+            }
+        };
+    }
+
+    deleteSelf(){
+        this.elements.form.remove();
+    }
+
+}
+
+export class VisitFormTherapist extends VisitForm{
+    renderDoctorSet(){
+        const {classListObj, textObj, attributesObj, elements} = this;
+        const defaultFormElements = super.renderVisitForm()
+
+        elements.age = super.renderInput("Возраст   : ",
+            {input: classListObj.input, label: classListObj.label},
+            "",
+            {
+                input: {
+                    min: "0",
+                    max: "120",
+                    title: "Введите значение от 0 до 120",
+                    required: true,
+                    type: "number",
+                    maxLength: "3",
+                    size: "3",
+                    name: "age"
+                }
+            })
+
+        defaultFormElements.card.age = {
+            label: "Возраст: ",
+            value: elements.age.children[0].value
+        };
+
+        defaultFormElements.card.doctor = {
+            label: "Доктор: ",
+            value: "Терапевт"
+        }
+
+        VisitForm.insertElementNextToAnotherElement(elements.reason, elements.age);
+
+        return{
+            ...defaultFormElements,
+            age: elements.age,
+        }
+    }
+
+}
+
+export class VisitFormDentist extends VisitForm {
+    renderDoctorSet() {
+        const {classListObj, textObj, attributesObj, elements} = this;
+        const defaultFormElements = super.renderVisitForm()
+
+        elements.previousVisitDate = super.renderInput("Дата последнего визита: ", //date of the previous appointment
+            {input: classListObj.input, label: classListObj.label},
+            "",
+            {
+                input: {
+                    type: "date",
+                    name: "date",
+                    required: true,
+                }
+            })
+
+        defaultFormElements.card.previousVisitDate ={
+            label:"Дата последнего визита: ",
+            value: elements.previousVisitDate.children[0].value
+        }
+
+        defaultFormElements.card.doctor = {
+            label: "Доктор: ",
+            value: "Стоматолог"
+        }
+
+        VisitForm.insertElementNextToAnotherElement(elements.reason, elements.previousVisitDate);
+        return {
+            ...defaultFormElements,
+            previousVisitDate: elements.previousVisitDate
+        }
+    }
+}
+
+export class VisitFormCardiologist extends VisitForm {
+    renderDoctorSet() {
+        const {classListObj, textObj, attributesObj, elements} = this;
+        const defaultFormElements = super.renderVisitForm();
+
+        elements.bloodPressure = super.renderInput("Обычное давление: ",
+            {input: classListObj.input, label: classListObj.label},
+            "",
+            {
+                input: {
+                    type: "text",
+                    max: "160",
+                    min: "50",
+                    title: "Введите значение между 50 и 160",
+                    maxLength: "6",
+                    size: "6",
+                    name: "bloodPressure",
+                    required: true,
+                }
+            });
+
+        elements.bmi = super.renderInput("Индекс массы тела: ", /*body mass index*/
+            {input: classListObj.input, label: classListObj.label},
+            "",
+            {
+                input: {
+                    type: "number",
+                    max: "60",
+                    min: "10",
+                    title: "Введите значение между 10 и 60",
+                    maxLength: "5",
+                    size: "5",
+                    name: "bmi",
+                    required: true,
+                }
+            });
+
+        elements.heartDiseases = super.renderInput("Перенесенные заболевания сердечно-сосудистой системы",
+            {input: classListObj.input, label: classListObj.label},
+            "",
+            {
+                input: {
+                    name: "diseases",
+                    required: true,
+                }
+            });
+
+        elements.age = super.renderInput("Возраст   : ",
+            {input: classListObj.input, label: classListObj.label},
+            "",
+            {
+                input: {
+                    min: "0",
+                    max: "120",
+                    title: "Введите значение от 0 до 120",
+                    required: true,
+                    type: "number",
+                    maxLength: "3",
+                    size: "3",
+                    name: "age"
+                }
+            })
+
+
+        elements.bloodPressure.addEventListener("keydown", event => {
+            let targetValue = event.target.value;
+            if (targetValue.length === 3) {
+                targetValue += "/"
+                event.target.value = targetValue;
+                if (event.code === "Backspace") {
+                    targetValue = ""
+                    event.target.value = targetValue
+                }
+            }
+
+        })
+
+        defaultFormElements.card.bloodPressure = {
+            label: "Обычное давление: ",
+            value: elements.bloodPressure.children[0].value
+        }
+
+         defaultFormElements.card.bmi = {
+            label: "Индекс массы тела: ",
+            value: elements.bmi.children[0].value
+        }
+
+         defaultFormElements.card.heartDisease = {
+            label: "Заболевания сердечно-сосудистой системы: ",
+            value: elements.heartDiseases.children[0].value
+        }
+
+         defaultFormElements.card.age = {
+            label: "Возраст: ",
+            value: elements.age.children[0].value
+        }
+
+        defaultFormElements.card.doctor = {
+            label: "Доктор: ",
+            value: "Кардиолог"
+        }
+
+
+        VisitForm.insertElementNextToAnotherElement(elements.reason, elements.previousVisitDate);
+        return {
+            ...defaultFormElements,
+            bloodPressure: elements.bloodPressure,
+            bmi: elements.bmi,
+            heartDiseases: elements.heartDiseases,
+            age: elements.age
+        }
+    }
+}
+
+
+
