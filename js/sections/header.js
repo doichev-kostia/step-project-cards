f53fbaea39a7725988d47cb85ee3d39c9010449e
Антон Молчанов <antonmolchanov97@gmail.com>
2021-04-30 13:41:30 +0300

    added an intermidiate filter commit

--- js/sections/header.js
+++ js/sections/header.js
@@ -1,24 +1,22 @@
 import API from "../components/API.js";
 import {Form, VisitForm, VisitFormDentist, VisitFormTherapist, VisitFormCardiologist} from "../components/Form.js";
-import {Visit,VisitDentist, VisitTherapist, VisitCardiologist} from "../components/Visit.js";
+import {Visit, VisitDentist, VisitTherapist, VisitCardiologist} from "../components/Visit.js";
 import DOMElement from "../components/DOMElement.js"
 import {ModalLogIn, ModalCreateVisit, ModalShowCard} from "../components/Modal.js";
 
 
 const root = document.querySelector('#root');
 
-function createFilter(parent){
+function createFilter(parent) {
 
     let select = new DOMElement('select', 'filterSelect', 'Выберите врача').render()
-    let selectConf = new DOMElement('button', 'selectConf', 'ok').render()
 
     let selectPriority = new DOMElement('select', 'filterSelect', 'Выберите срочность').render()
-    let selectPriorityBtn = new DOMElement('button', 'selectConf', 'ok').render()
 
     let searchByDescription = new DOMElement('input', 'filterSearch', 'Поиск по описанию').render()
     let searchByDescriptionBtn = new DOMElement('button', 'filterSearchBtn', 'Поиск').render()
 
-    parent.prepend(select, selectConf,selectPriority,selectPriorityBtn,searchByDescription,searchByDescriptionBtn);
+    parent.prepend(select, selectPriority, searchByDescription, searchByDescriptionBtn);
 
     //элементы фильтра поиска по врачу
     let optionAll = new DOMElement('option', 'filterOption', 'Все').render()
@@ -37,45 +35,99 @@ function createFilter(parent){
     //конец элементов фильтра поиска по срочности
 
 
+    let doctorArray;
+    let priorityArray;
+    let keyWordsArray;
+    let arrayOfAllIDs;
+    let uniqueIDs;
 
 
-    // let selectPriority = new DOMElement('select', 'filterSelect', 'Выберите срочность').render()
-    // let selectPriorityBtn = new DOMElement('button', 'selectConf', 'ok').render()
 
-    selectConf.addEventListener('click', async () => {
-        document.querySelectorAll('.card').forEach(card => card.classList.remove('hidden'))
+    searchByDescriptionBtn.addEventListener('click', async () => {
+        document.querySelectorAll('.card').forEach(card => card.classList.add('hidden'))
+        document.querySelectorAll('.card').forEach(function (card){
+            card.classList.remove('matchesTwocriterias')
+        })
+        uniqueIDs = [];
+        arrayOfAllIDs = [];
+        doctorArray = []//
+        priorityArray = []
+        keyWordsArray = []
+
+
+
         let doctor = select.value
         await API.getAllCards().then(response => response.forEach(function (object) {
-            if (doctor === 'Все') {
-                return
+                if (doctor === 'Все') {
+                    doctorArray.push(object.id)
+                } else if (object.doctor.value === 'Стоматолог') {
+                    if (doctor === 'Стоматолог') {
+                        doctorArray.push(object.id)
+                    }
+                } else if (object.doctor.value === 'Терапевт') {
+                    if (doctor === 'Терапевт') {
+                        doctorArray.push(object.id)
+                    }
+                } else if (object.doctor.value === 'Кардиолог') {
+                    if (doctor === 'Кардиолог') {
+                        doctorArray.push(object.id)
+                    }
+                }
             }
-            else if (object.doctor.value !== doctor) {
-                document.getElementById(`${object.id}`).classList.add('hidden');
-            }
-        }))
-    })
-    selectPriorityBtn.addEventListener('click',async ()=>{
-        document.querySelectorAll('.card').forEach(card => card.classList.remove('hidden'))
+        ))
+        console.log('doctors: ' + doctorArray)
+
         let priority = selectPriority.value;
-        await API.getAllCards().then(response => response.forEach(function (object){
-            console.log(object)
+        await API.getAllCards().then(response => response.forEach(function (object) {
             if (priority === 'all') {
-                return
-            } else if(object.priority.value !== priority){
-                document.getElementById(`${object.id}`).classList.add('hidden');
+                priorityArray.push(object.id)
+            } else if (object.priority.value === 'low') {
+                if (priority === 'low') {
+                    priorityArray.push(object.id)
+                }
+            } else if (object.priority.value === 'normal') {
+                if (priority === 'normal') {
+                    priorityArray.push(object.id)
+                }
+            } else if (object.priority.value === 'high') {
+                if (priority === 'high') {
+                    priorityArray.push(object.id)
+                }
             }
         }))
-    })
-    searchByDescriptionBtn.addEventListener('click',async ()=>{
-        document.querySelectorAll('.card').forEach(card => card.classList.remove('hidden'))
+        console.log('priority: ' + priorityArray)
+
+        // document.querySelectorAll('.card').forEach(card => card.classList.remove('hidden'))
         let searchValue = searchByDescription.value
-        await API.getAllCards().then(response => response.forEach(function (object){
-            if (searchValue !== object.description.value){
-                document.getElementById(`${object.id}`).classList.add('hidden');
+        await API.getAllCards().then(response => response.forEach(function (object) {
+            if (searchValue === object.description.value) {
+                keyWordsArray.push(object.id)
             }
         }))
+        console.log('keywords: ' + keyWordsArray)
+
+        arrayOfAllIDs = doctorArray.concat(priorityArray,keyWordsArray)
+        arrayOfAllIDs.sort()
+        console.log('all IDs: ' +arrayOfAllIDs)
+
+            for (let i =0; i < arrayOfAllIDs.length; i++){
+                let test = arrayOfAllIDs.filter(item => item === arrayOfAllIDs[i])
+                // console.log(test)
+                if (test.length === 3){
+                    document.getElementById(`${test[0]}`).classList.remove('matchesTwocriterias')
+                    // document.getElementById(`${test[0]}`).classList.add('matchesThreeCriterias')
+                    document.getElementById(`${test[0]}`).classList.remove('hidden')
+
+                }
+                 else if (test.length === 2){
+                    document.getElementById(`${test[0]}`).classList.add('matchesTwocriterias')
+                    document.getElementById(`${test[0]}`).classList.remove('hidden')
+                }
+            }
     })
 }
+
+
 
 export default async function createHeaderSection() {
     const header = new DOMElement("header", ["header", "wrapper"]).render();
@@ -84,7 +136,7 @@ export default async function createHeaderSection() {
     const main = new DOMElement("main", "main").render();
     const cardSection = new DOMElement("section", ["visit-section", "wrapper"]).render()
     const noVisitMessage = new DOMElement("p", "no-visit-message", "Визитов не добавлено", {hidden: true}).render();
-    let filterContainer = new DOMElement('div', ['filter-section','wrapper'], '').render()
+    let filterContainer = new DOMElement('div', ['filter-section', 'wrapper'], '').render()
 
 
     root.append(header, main);
@@ -185,16 +237,16 @@ function renderCards(parent, cards) {
     noVisitMessage.hidden = true;
 
     cards.forEach(card => {
-      if(card.doctor.value === "Терапевт"){
-          const therapistCard = new VisitTherapist(card).renderCard();
-          parent.append(therapistCard.cardContainer);
-      }else if(card.doctor.value === "Кардиолог"){
+        if (card.doctor.value === "Терапевт") {
+            const therapistCard = new VisitTherapist(card).renderCard();
+            parent.append(therapistCard.cardContainer);
+        } else if (card.doctor.value === "Кардиолог") {
             const cardiologistCard = new VisitCardiologist(card).renderCard();
             parent.append(cardiologistCard.cardContainer);
-      } else if(card.doctor.value === "Стоматолог"){
-          const dentistCard = new VisitDentist(card).renderCard();
-          parent.append(dentistCard.cardContainer);
-      }
+        } else if (card.doctor.value === "Стоматолог") {
+            const dentistCard = new VisitDentist(card).renderCard();
+            parent.append(dentistCard.cardContainer);
+        }
     })
 }
 
@@ -290,7 +342,7 @@ async function createVisitCard(formElements) {
 
     for (let [key, value] of Object.entries(formElementsObj)) {
 
-            visitDetails[key].value = formElementsObj[key].children[0].value;
+        visitDetails[key].value = formElementsObj[key].children[0].value;
     }
 
     let response = await API.saveCard(visitDetails);
